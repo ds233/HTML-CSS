5ed6c484dcfdf34865a47cc8725c4a06f0cb6d4d
Jack Sparrow <eugen-vorobiev@yandex.ru>
2023-02-25 19:01:57 +0300

    Homework 10 done

---
+++ JS-hw10/data.js
@@ -0,0 +1,27 @@
+//Task 1
+//const data = `[{ "id": 67494, "answer": "Jack London", "question": "A California park named for this author contains the ruins of Wolf House, the mansion he built but never lived in", "value": 500, "airdate": "2000-02-23T20:00:00.000Z", "created_at": "2022-12-30T19:07:02.423Z", "updated_at": "2022-12-30T19:07:02.423Z", "category_id": 6209, "game_id": 2036, "invalid_count": null, "category": { "id": 6209, "title": "state parks", "created_at": "2022-12-30T19:07:02.132Z", "updated_at": "2022-12-30T19:07:02.132Z", "clues_count": 20 } }]`
+
+//Task 2
+
+const dt = `[
+    {
+        "image": "./img/img1.jpg",
+        "alt": "photo of the product 1",
+        "header": "Bestseller 1",
+        "text": "This is bestseller product of december 2022"
+    },
+
+    {
+        "image": "./img/img2.jpg",
+        "alt": "photo of the product 2",
+        "header": "Bestseller 2",
+        "text": "This is bestseller product of january 2022"
+    },
+
+    {
+        "image": "./img/img3.jpg",
+        "alt": "photo of the product 3",
+        "header": "Bestseller 3",
+        "text": "This is bestseller product of february 2023"
+    }
+]`
