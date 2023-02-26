cf0018e52d88a55b3ded59c854ca35e668c91843
Jack Sparrow <eugen-vorobiev@yandex.ru>
2023-02-26 18:23:03 +0300

    Homework 11 done

---
+++ JS-hw11/data.js
@@ -0,0 +1,62 @@
+const dt = `[
+    {
+        "image": "./pic/fi-card1.jpg",
+        "alt": "Featured items card",
+        "imageHov": "./pic/FI-Hover.png",
+        "altHov": "",
+        "title": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+
+    {
+        "image": "./pic/fi-card2.png",
+        "alt": "Featured items card",
+        "imageHov": "./pic/FI-Hover.png",
+        "altHov": "",
+        "title": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+
+    {
+        "image": "./pic/fi-card3.png",
+        "alt": "Featured items card",
+        "imageHov": "./pic/FI-Hover.png",
+        "altHov": "",
+        "title": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+
+    {
+        "image": "./pic/fi-card4.png",
+        "alt": "Featured items card",
+        "imageHov": "./pic/FI-Hover.png",
+        "altHov": "",
+        "title": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+
+    {
+        "image": "./pic/fi-card5.png",
+        "alt": "Featured items card",
+        "imageHov": "./pic/FI-Hover.png",
+        "altHov": "",
+        "title": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+
+    {
+        "image": "./pic/fi-card6.png",
+        "alt": "Featured items card",
+        "imageHov": "./pic/FI-Hover.png",
+        "altHov": "",
+        "title": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    }
+
+]`
